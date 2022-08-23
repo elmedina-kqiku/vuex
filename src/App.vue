38ee3a5c84749c9ef61cbafe9ec3e35763f8ce93
elmedinakqiku1 <elmedinakqiku@gmail.com>
2022-08-23 11:11:12 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <base-container title="Vuex">
+    <TheCounter></TheCounter>  
+    <FavoriteValue></FavoriteValue>
+    <button @click="addOne">Add 10</button>
+    <ChangeCounter></ChangeCounter>
+  </base-container>
+
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import TheCounter from './components/TheCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
+
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavoriteValue,
+    
+},
+  methods: {
+    addOne() {
+       // this.$store.commit('increase', { value: 10 });
+      this.$store.commit({
+        type: 'increase',
+        value: 10,
+      });
+    }
+  }
+  
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
